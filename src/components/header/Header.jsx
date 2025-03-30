d761c51b117b7364641e6fc16aa855654ab51367
Ivan Pakhomov <ip1133@ya.ru>
2025-03-30 20:12:01 +0300

    ref: removed unnecessary decomposition

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
-import { Logo } from './Logo';
+import widgetLogo from '../../assets/widget-logo.png';
 
 export function Header() {
   return (
     <HeaderContainer>
-      <Logo />
+      <StyledLogo src={widgetLogo} alt="logo" />
     </HeaderContainer>
   );
 }
@@ -14,4 +14,13 @@ const HeaderContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const StyledLogo = styled.img`
+  max-width: 300px;
+  user-select: none;
+
+  @media (max-width: 930px) {
+    margin-bottom: 20px;
+  }
 `;
